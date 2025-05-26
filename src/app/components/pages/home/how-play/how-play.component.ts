24e6b60be50d57b6ec9350a35e5d352deb72c147
AllanCordova <allan.2024@alunos.utfpr.edu.br>
2025-05-26 11:21:16 -0300

    alterado mantendo apenas documentação

--- src/app/components/pages/home/how-play/how-play.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-how-play',
-  templateUrl: './how-play.component.html',
-  styleUrls: ['./how-play.component.css'],
-})
-export class HowPlayComponent {}
