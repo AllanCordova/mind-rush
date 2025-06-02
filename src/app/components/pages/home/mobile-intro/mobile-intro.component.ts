4964c58f2fe5915f8eb1e8b2079322dfb32f9218
AllanCordova <allan.2024@alunos.utfpr.edu.br>
2025-06-02 19:48:35 -0300

    Implementado, adições na versão mobile corrigido links fantasmas

--- src/app/components/pages/home/mobile-intro/mobile-intro.component.ts
+++ src/app/components/pages/home/mobile-intro/mobile-intro.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TitleComponent } from '../title/title.component';
 import { RouterModule } from '@angular/router';
+import { ImgServiceService } from '../../../../services/img-service.service';
 
 @Component({
   selector: 'app-mobile-intro',
@@ -8,4 +9,25 @@ import { RouterModule } from '@angular/router';
   templateUrl: './mobile-intro.component.html',
   styleUrl: './mobile-intro.component.css',
 })
-export class MobileIntroComponent {}
+export class MobileIntroComponent implements OnInit {
+  public imgPathList: string[] = [];
+
+  public constructor(private _imgService: ImgServiceService) {}
+
+  private getPathImg(): string {
+    return this._imgService.getRandomImg();
+  }
+
+  private setImgPathList(size: number): void {
+    while (this.imgPathList.length < size) {
+      const img: string = this.getPathImg();
+      if (!this.imgPathList.includes(img)) {
+        this.imgPathList.push(img);
+      }
+    }
+  }
+
+  public ngOnInit(): void {
+    this.setImgPathList(2);
+  }
+}
